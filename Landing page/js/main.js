64d855563e9fdb543ba16fcb3bffe53c90d9c975
OlehYavoriv <73217391+OlehYavoriv@users.noreply.github.com>
2021-01-25 22:44:50 +0200

    Add files via upload

---
+++ Landing page/js/main.js
@@ -0,0 +1,14 @@
+$(document).ready(function(){
+$('.logo-link,.menu-link').each(function(index, element){
+$(element).click(function(){
+const id=$(this).attr('href');
+// console.log(id);
+const top=$(id).offset().top;
+// console.log(top);
+$('html').animate({
+    scrollTop :top
+},1000);
+
+})
+})
+})
